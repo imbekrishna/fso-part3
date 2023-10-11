82d2eec67d516d4f30edef9727f3a773c459a589
be-krishna <bkshgnj@gmail.com>
2023-10-11 13:01:50 +0530

    part3c-ex-3.14 done | added mongoose

--- index.js
+++ index.js
@@ -1,98 +1,107 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const { generateId, personExists } = require("./helpers/persons");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(express.json());
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :body"
+    )
 );
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
 ];
 
 app.get("/info", (request, response) => {
-  const count = persons.length;
-  const date = new Date();
-  response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
+    const count = persons.length;
+    const date = new Date();
+    response.send(
+        `<p>Phonebook has info for ${count} people</p><p>${date}</p>`
+    );
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+    const id = Number(request.params.id);
 
-  const person = persons.find((p) => p.id === id);
+    const person = persons.find((p) => p.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 app.post("/api/persons", (request, response) => {
-  const body = request.body;
+    const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({ error: "name missing" });
-  }
-  if (!body.number) {
-    return response.status(400).json({ error: "number missing" });
-  }
-  if (personExists(persons, body.name)) {
-    return response.status(400).json({ error: "name must be unique" });
-  }
+    if (!body.name) {
+        return response.status(400).json({ error: "name missing" });
+    }
+    if (!body.number) {
+        return response.status(400).json({ error: "number missing" });
+    }
 
-  const newPerson = {
-    ...body,
-    id: generateId(),
-  };
+    // if (personExists(persons, body.name)) {
+    //     return response.status(400).json({ error: "name must be unique" });
+    // }
 
-  persons = persons.concat(newPerson);
+    const newPerson = Person({
+        name: body.name,
+        number: body.number,
+    });
 
-  response.json(newPerson);
+    newPerson.save().then((person) => {
+        response.json(person);
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+    const id = Number(request.params.id);
 
-  persons = persons.filter((p) => p.id !== id);
+    persons = persons.filter((p) => p.id !== id);
 
-  response.status(204).end();
+    response.status(204).end();
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+    response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
